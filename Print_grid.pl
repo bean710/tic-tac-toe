5a03dc51eba4bd5611c6e59ebfd3c68667374a8e
Ben Keener <keener4christ@gmail.com>
2020-05-09 01:57:18 -0400

    Minor fixes

--- Print_grid.pl
+++ Print_grid.pl
@@ -1,3 +1,4 @@
+=pod
 sub Print_grid {
     print "Board: \n";
     my @GameBoard = @{$_[0]};
@@ -9,4 +10,35 @@ sub Print_grid {
 		print("\n")
     }
 }
+=cut
+sub Print_grid {
+	print "\033[2J";    #clear the screen
+	print "\033[0;0H";
+    print "Board: \n";
+    my @GameBoard = @{$_[0]};
+    my $length = scalar @GameBoard;
+    for(my $m = 0; $m < $length; $m++) {
+		for(my $n = 0; $n < $length; $n++) {
+			if ($n != 0) {
+				print "| ";
+			} else {
+				print " ";
+			}
+			my $out = " ";
+			my $gbr = $GameBoard[$m][$n];
+			if ($gbr == 1)
+			{
+				$out = "X";
+			} elsif ($gbr == 2) {
+				$out = "O";
+			}
+			print("$out ");
+		}
+		print("\n");
+		if ($m != 2){
+			print "---+---+---\n";
+		}
+    }
+}
+
 1;
--- ttt.pl
+++ ttt.pl
@@ -26,142 +26,6 @@ my $player2 = "O";
 #	'20' => 0, '21' => 0, '22' => 0
 #};
 
-=pod
-sub MakeMove {
-	my ($x, $y, $player) = @_;
-
-	if ($x < 0 or $x > 2 or $y < 0 or $y > 2) {
-		return 0;
-	}
-
-	if ($player == 2) {
-		print "Computer made move $x, $y\n";
-	}
-
-	#my $coord = $x.$y."";
-	#print "Coord: $coord\n";
-	#my $val = $GB{'00'};
-	my $val = $GameBoard[$x][$y];
-	#print "On val: $val\n";
-	if ($val != 0) {
-		return 0;
-	} else {
-		$GameBoard[$x][$y] = $player;
-		return 1;
-	}
-
-	return 0;
-}
-
-sub Print_grid {
-    print "Board: \n";
-    my $length = scalar @GameBoard;
-    for(my $m = 0; $m < $length; $m++) {
-		for(my $n = 0; $n < $length; $n++) {
-			print("$GameBoard[$m][$n] ");
-		}
-		print("\n")
-    }
-}
-
-sub MakeSmartRobotMove {
-	my $i = 0;
-
-	#Checking if robot can make a move to win
-	foreach $i (0..2) {
-		my $n = CheckSetRobot($GameBoard[$i][0], $GameBoard[$i][1], $GameBoard[$i][2]);
-		if ($n != -1) {
-			MakeMove($i, $n, 2);
-			return 1;
-		}
-	}
-
-	foreach $i (0..2) {
-		my $n = CheckSetRobot($GameBoard[0][$i], $GameBoard[1][$i], $GameBoard[2][$i]);
-		if ($n != -1) {
-			MakeMove($n, $i, 2);
-			return 1;
-		}
-	}
-
-	my $n = CheckSetRobot($GameBoard[0][0], $GameBoard[1][1], $GameBoard[2][2]);
-	if ($n != -1) {
-		MakeMove($n, $n, 2);
-		return 1;
-	}
-
-	$n = CheckSetRobot($GameBoard[0][2], $GameBoard[1][1], $GameBoard[2][0]);
-	if ($n != -1) {
-		MakeMove($n, 2 - $n, 2);
-		return 1;
-	}
-
-	#Checking if robot can make move to block player
-	foreach $i (0..2) {
-		my $n = CheckSetPlayer($GameBoard[$i][0], $GameBoard[$i][1], $GameBoard[$i][2]);
-		if ($n != -1) {
-			MakeMove($i, $n, 2);
-			return 1;
-		}
-	}
-
-	foreach $i (0..2) {
-		my $n = CheckSetPlayer($GameBoard[0][$i], $GameBoard[1][$i], $GameBoard[2][$i]);
-		if ($n != -1) {
-			MakeMove($n, $i, 2);
-			return 1;
-		}
-	}
-
-	$n = CheckSetPlayer($GameBoard[0][0], $GameBoard[1][1], $GameBoard[2][2]);
-	if ($n != -1) {
-		MakeMove($n, $n, 2);
-		return 1;
-	}
-
-	$n = CheckSetPlayer($GameBoard[0][2], $GameBoard[1][1], $GameBoard[2][0]);
-	if ($n != -1) {
-		MakeMove($n, 2 - $n, 2);
-		return 1;
-	}
-
-	#Make dumb move
-	return MakeRobotMove();
-}
-
-sub MakeRobotMove {
-    my $length = scalar @GameBoard;
-    for(my $m = 0; $m < $length; $m++) {
-		for(my $n = 0; $n < $length; $n++) {
-			if ($GameBoard[$m][$n] == 0) {
-				MakeMove($m, $n, 2);
-				return 1;
-			}
-		}
-    }
-	return 0;
-}
-
-sub Print_grid {
-    print "Board: \n";
-    my $length = scalar @GameBoard;
-    for(my $m = 0; $m < $length; $m++) {
-		for(my $n = 0; $n < $length; $n++) {
-			my $gbr = $GameBoard[$m][$n];
-			if ($gbr == 1){
-				print "$player1 ";
-			} elsif ($gbr == 2) {
-				print "$player2 ";
-			} else {
-				print "* ";
-			}
-		}
-		print("\n")
-    }
-}
-=cut
-
-
 sub Main {
 	print "VS CPU [1] or VS Player [2] or : ";
 	my $choice = <STDIN>;
@@ -175,7 +39,11 @@ sub Main {
 
 	my $ret = "";
 	my $isGame = 1;
+	Print_grid(\@GameBoard);
 	while ($isGame) {
+		if ($choice == 2) {
+			print "Player $turn\'s turn\n";
+		}
 		print "Enter x coordinate: ";
 		my $x = <STDIN>;
 		print "Enter y coordinate: ";
@@ -184,24 +52,21 @@ sub Main {
 		$x =~ s/[^0-9]*//g;
 		$y =~ s/[^0-9]*//g;
 
-		if ($choice == 1) {
-			$ret = MakeMove($y, $x, 1, \@GameBoard);
-		}
-		elsif ($choice == 2) {
-			if ($turn == 1) {	
-			$ret = MakeMove($x, $y, $turn, \@GameBoard);	
-			$turn = 2;	
-			}	
-			elsif ($turn == 2) {	
-				$ret = MakeMove($x, $y, $turn, \@GameBoard);	
-				$turn = 1;	
-			}
-		}
+		$ret = MakeMove($y, $x, 1, \@GameBoard);
 
 		if ($ret == 0) {
-			print "Invalid move! Try again.\n";
 			Print_grid(\@GameBoard);
+			print "Invalid move! Try again.\n";
 			next;
+		}
+
+		if ($choice == 2) {
+			if ($turn == 1) {	
+				$turn = 2;	
+			}	
+			elsif ($turn == 2) {	
+				$turn = 1;	
+			}
 		}
 		#print "You made move at x:$x y:$y\n";
 
